c55b857ac21f693f9fb05b2144e82828a71b896a
XavierCanjura <36569554+XavierCanjura@users.noreply.github.com>
2021-10-23 19:45:41 -0600

    login conectado con firebase

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, SafeAreaView, StatusBar, LogBox, Text } from 'react-native';
+import base64 from 'react-native-base64';
+import Auth from './src/components/Auth';
+import firebase from './src/utils/firebase';
+import 'firebase/auth';
+
+function btoa(data) {
+  return new base64(data, "binary").toString("base64");
+}
+
+function atob(data) {
+  return new base64(data, "base64").toString("binary");
+}
+
+LogBox.ignoreAllLogs(['Setting a timer']);
 
 export default function App() {
+  const [user, setUser] = useState(undefined);
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((response) => {
+      setUser(response);
+    });
+  }, []);
+  
+  if (user === undefined) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView style={styles.background}>
+        { user ? <Text style = {{ fontSize: 40, textAlign: 'center', color: '#FFF' }} >Bienvenido</Text>  : <Auth /> }
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  background: {
+  backgroundColor: '#15212b',
+  height: '100%',
   },
-});
+  });
---
+++ src/utils/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCU0N4fsGkdKjq_P9q55zbyqHRTcTCeQCo",
+  authDomain: "clase11-c233f.firebaseapp.com",
+  projectId: "clase11-c233f",
+  storageBucket: "clase11-c233f.appspot.com",
+  messagingSenderId: "859078333573",
+  appId: "1:859078333573:web:71456ebf9bd26b196945ec"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
